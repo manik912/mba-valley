ead1061971dda6f132623b2870ac6c6713be5de7
Manik <guptamanik1921@gmail.com>
2020-08-26 18:49:38 +0530

    update

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    catagory = models.CharField(max_length=100)
+    content = models.TextField(max_length=7000)
+    date_published = models.DateTimeField(default=timezone.now)
+    blog_image = models.ImageField(default='default.jpg', upload_to='blog_images', blank=True)
+    
+    def __str__(self):
+        return self.title
--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import PostDetailView, SearchView
+
 
 urlpatterns = [
     path('',			views.home,		name='home'),
+    path('blogs/<int:pk>/', 		PostDetailView.as_view(), 	name='post-detail'),
+    path('results/', SearchView.as_view(template_name='home/search.html'), name='search'),
 ]
 if settings.DEBUG: 
 	urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT )
