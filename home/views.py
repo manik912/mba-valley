eb2fc38908b36307c8063a7cf46a2a36f8cac0ab
tanya1810 <tanya.sood.311@gmail.com>
2020-08-27 19:51:19 +0530

    search, popular

--- home/views.py
+++ home/views.py
@@ -5,26 +5,39 @@ from .models import Post
 # Create your views here.
 def home(request):
     posts = Post.objects.order_by('-date_published')
+    popular = Post.objects.filter(popular=True)
 
     context = {
-        'posts': posts
+        'posts': posts,
+        'popular':popular
     }
     return render(request, 'home/home.html', context)
 
-class SearchView(ListView):
-    model = Post
-    template_name = 'search.html'
-    context_object_name = 'Post.objects.all'
+# class SearchView(ListView):
+#     model = Post
+#     template_name = 'search.html'
+#     context_object_name = 'Post.objects.all'
 
-    def get_queryset(self):
-       result = super(SearchView, self).get_queryset()
-       query = self.request.GET.get('search')
-       if query:
-          postresult = Post.objects.filter(title__contains=query)
-          result = postresult
-       else:
-           result = None
-       return result
+#     def get_queryset(self):
+#        result = super(SearchView, self).get_queryset()
+#        query = self.request.GET.get('search')
+#        if query:
+#           postresult = Post.objects.filter(title__contains=query)
+#           result = postresult
+#        else:
+#            result = None
+#        return result
+
+def SearchView(request):
+    query = request.GET.get("search", None)
+    qs = Post.objects.all()
+    if query is not None:
+        qs = qs.filter(title__icontains=query)
+    context = {
+        "posts": qs,
+    }
+    return render(request, 'home/search.html', context)
+
 
 
 class PostDetailView(DetailView):
