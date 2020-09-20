68673305fda67f34ed0d0d90eef9bdc056333641
N15H4N7 <ngupta_be19@thapar.edu>
2020-09-20 22:31:17 +0530

    Competition Model

--- competition/models.py
+++ competition/models.py
@@ -9,9 +9,9 @@ class competition(object):
 	name 			= models.CharField(max_length = 20)
 	about 			= models.CharField(max_length = 200)
 	last_date 		= models.DateTimeField(default = timezone.now)
+	begin_date 		= models.DateTimeField(default = timezone.now)
 	min_size 		= models.IntegerField(default = 1)
 	max_size 		= models.IntegerField(default = 1)
-	rounds 			= models.IntegerField(default = 1)
 	prizes 			= models.ForeignKey(prizes, related_name = 'prize', on_delete = models.CASCADE)
 	image 			= models.ImageField(default = 'competition/default.png', upload_to = 'competition/')
 
@@ -21,17 +21,11 @@ class competition(object):
 class register(object):
 	"""docstring for register"""
 	event 			= models.ForeignKey(competition, related_name = 'competition', on_delete = models.CASCADE, null = True)
+	team_leader 	= models.ForeignKey(User, related_name = 'participants', on_delete = models.CASCADE, null = True)
 
 class prizes(object):
 	"""docstring for prizes"""
-	def __init__(self, arg):
-		super(prizes, self).__init__()
-		self.arg = arg
-		
+	event 			= models.ForeignKey(competition, related_name = 'event', on_delete = models.CASCADE, null = True)
+	title 			= models.CharField(max_length = 20)
+	reward 			= models.CharField(max_length = 20)
 
-class Rounds(object):
-	"""docstring for Rounds"""
-	def __init__(self, arg):
-		super(Rounds, self).__init__()
-		self.arg = arg
-		
