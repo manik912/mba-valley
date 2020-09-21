1f007b84fe9da5795f227c4a58e6c0126e2f8920
N15H4N7 <ngupta_be19@thapar.edu>
2020-09-22 00:11:33 +0530

    Competition Page linked to Front End

--- competition/views.py
+++ competition/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import competition
+from .models import competition, prizes
+from .models import prizes as Prizes
 
 # Create your views here.
 def competitionDetailView(request):
@@ -8,6 +9,11 @@ def competitionDetailView(request):
 
 	context = {
 		'Competition' : Competition,
+		'Prizes' : Prizes.objects.filter(event = Competition)
 	}
+
+	print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+	print(Competition.organiser.name)
+	print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
 
 	return render(request, 'competition/competition_detail.html', context)
