45adb0fa9fd9ff8c8b95c34f6ffa02e0bd985c63
N15H4N7 <ngupta_be19@thapar.edu>
2020-09-21 13:07:26 +0530

    asdf

--- competition/views.py
+++ competition/views.py
@@ -4,10 +4,10 @@ from .models import competition
 # Create your views here.
 def competitionDetailView(request):
 	
-	Competition = competition.objects.filter(id=1) 
+	Competition = competition.objects.filter(id=1).first()
 
 	context = {
 		'Competition' : Competition,
 	}
 
-	return render(request, 'home/competition_detail.html', context)
+	return render(request, 'competition/competition_detail.html', context)
