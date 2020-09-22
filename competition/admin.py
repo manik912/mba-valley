a69cddc17a7028a6b8794774e9ee52d57203d89a
N15H4N7 <ngupta_be19@thapar.edu>
2020-09-22 16:30:09 +0530

    Submit Model Done

--- competition/admin.py
+++ competition/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import competition,prizes
+from .models import competition, prizes, submit
 
 # Register your models here.
 admin.site.register(competition)
 admin.site.register(prizes)
+admin.site.register(submit)
